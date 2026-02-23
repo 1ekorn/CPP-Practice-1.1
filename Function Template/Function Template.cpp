632338c562d15a14b9d9f461715e12e90d495da6
1ekorn <1biletskyi.m@gmail.com>
2026-02-23 21:41:45 +0000

    Add function template practice

---
+++ Function Template/Function Template.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+template <typename T>
+void showValue(T value);
+
+template <typename T>
+void showValue(T arr[], int size, char symbol);
+
+template <typename T>
+void initializeArr(T array[], int size);
+
+int getRandNum();
+
+int main()
+{
+	srand(static_cast<unsigned>(time(nullptr)));
+	const int SIZE = 20;
+	int arr[SIZE];
+
+	showValue(15);
+	showValue('f');
+	showValue(15.5);
+
+	std::cout << '\n';
+
+	initializeArr(arr, SIZE);
+	showValue(arr, SIZE, '-');
+}
+
+template <typename T>
+void showValue(T value)
+{
+	std::cout << value << '\n';
+}
+
+template <typename T>
+void showValue(T arr[], int size, char symbol)
+{
+	for (int i = 0; i < size; i++) 
+	{
+		std::cout << arr[i];
+			if (i != size - 1)
+				std::cout << symbol;
+	}
+	std::cout << '\n';
+}
+
+template <typename T>
+void initializeArr(T array[], int size)
+{
+	for (int i = 0; i < size; i++)
+		array[i] = getRandNum();
+}
+
+int getRandNum()
+{
+	return rand() % 100;
+}
